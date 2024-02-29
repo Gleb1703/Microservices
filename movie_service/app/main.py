d9c302871cd64a2c162c1aab1f4efb1f19ce2179
kl <nightmare090@mail.ru>
2024-02-29 22:30:58 +0300

    Update main.py

--- movie_service/app/main.py
+++ movie_service/app/main.py
@@ -69,55 +69,66 @@ def user_got_role():
 
 @app.get("/health", status_code=status.HTTP_200_OK)
 async def service_alive():
-    return {'message': 'service alive'}
+    if (user_got_role()):
+        return {'message': 'service alive'}
+    else:
+        return "Wrong JWT Token"
 
 
 @app.get("/get_movies")
 async def get_movies(db: db_dependency):
-    try:
-        result = db.query(MovieDB).limit(100).all()
-        return result
-    except Exception as e:
-        return "Cant access database!"
-
+    if (user_got_role()):
+        try:
+            result = db.query(MovieDB).limit(100).all()
+            return result
+        except Exception as e:
+            return "Cant access database!"
+    else:
+        return "Wrong JWT Token"
 
 @app.get("/get_movie_by_id")
 async def get_movie_by_id(movie_id: int, db: db_dependency):
-    try:
-        result = db.query(MovieDB).filter(MovieDB.id == movie_id).first()
+    if (user_got_role()):
+        try:
+            result = db.query(MovieDB).filter(MovieDB.id == movie_id).first()
+            return result
+        except Exception as e:
+            raise HTTPException(status_code=404, detail="Movie not found")
         return result
-    except Exception as e:
-        raise HTTPException(status_code=404, detail="Movie not found")
-    return result
-
+    else:
+        return "Wrong JWT Token"
 
 @app.post("/add_movie")
 async def add_movie(movie: Movie, db: db_dependency):
-    try:
-        movie_db = MovieDB(
-            id=movie.id,
-            movie_name=movie.movie_name,
-            creation_date=movie.creation_date,
-            genre=movie.genre,
-            director=movie.director
-        )
-        db.add(movie_db)
-        db.commit()
-        return movie_db
-    except Exception as e:
-        raise HTTPException(status_code=404, detail="Movie not found")
-
+    if (user_got_role()):
+        try:
+            movie_db = MovieDB(
+                id=movie.id,
+                movie_name=movie.movie_name,
+                creation_date=movie.creation_date,
+                genre=movie.genre,
+                director=movie.director
+            )
+            db.add(movie_db)
+            db.commit()
+            return movie_db
+        except Exception as e:
+            raise HTTPException(status_code=404, detail="Movie not found")
+    else:
+        return "Wrong JWT Token"
 
 @app.delete("/delete_movie")
 async def delete_movie(movie_id: int, db: db_dependency):
-    try:
-        movie_db = db.query(MovieDB).filter(MovieDB.id == movie_id).first()
-        db.delete(movie_db)
-        db.commit()
-        return "Success"
-    except Exception as e:
-        return "Cant find movie"
-
+    if (user_got_role()):
+        try:
+            movie_db = db.query(MovieDB).filter(MovieDB.id == movie_id).first()
+            db.delete(movie_db)
+            db.commit()
+            return "Success"
+        except Exception as e:
+            return "Cant find movie"
+    else:
+        return "Wrong JWT Token"
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=int(os.getenv('PORT', 80)))
